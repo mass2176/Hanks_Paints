720fcba23c7a388da88fe2b01e144e5e7e09a228
Jeremy <jpjohnson@borgwarner.com>
2026-06-27 09:49:09 -0400

    updated allow_origins in main.py for teting

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,7 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(title="Hanks Paints MVP API")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
